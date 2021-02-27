97dec87f70b6bbc0675cb50581843164f04cb25f
ludanekrasova <ludanekrasova@users.noreply.github.com>
2021-02-27 16:56:07 +0500

    updated files

--- lamp/index.php
+++ lamp/index.php
@@ -2,9 +2,55 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<h1>Оценки</h1>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Оценки</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<style>
+body {
+  padding-top: 50px;
+}
+.starter-template {
+  padding: 40px 15px;
+  text-align: center;
+}
+
+.table td {
+   text-align: center;
+}
+
+</style>
+
 </head>
 <body>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Оценки</a>
+    </div>
+    <div id="navbar" class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Главная</a></li>
+        <li><a href="#about">О проекте</a></li>
+        <li><a href="#contact">Контакты</a></li>
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+</nav>
+
+<div class="container">
+
+    <div class="starter-template">
+    <h1>Оценки</h1>
 
 <?php
 
@@ -29,15 +75,18 @@ SELECT name, surname, subject, grade, date, faculty from people
     JOIN faculties on grades.faculty_id = faculties.id
 ';
 
+//$sql = 'select * from people';
+
 $result = pg_query($db, $sql);
 
 
 function caption($key) {
-    $keys = array("name"=>"Имя","surname"=>"Фамилия","subject"=>"Предмет","grade"=>"Оценка",'date'=>'Дата');
+    $keys = array('name'=>'Имя','surname'=>'Фамилия','subject'=>'Предмет',
+        'grade'=>'Оценка','date'=>'Дата', 'faculty'=>'Факультет');
     return isset($keys[$key]) ? $keys[$key] : $key;
 }
 
-echo "<table border=1>\n";
+echo "<table class='table table-hover'>\n";
 
 $fields = pg_num_fields($result);
 for ($i = 0; $i < $fields; $i++) {
@@ -58,5 +107,7 @@ echo "</table>\n";
 
 
 ?>
+    </div>
+</div><!-- /.container -->
 </body>
 </html>
